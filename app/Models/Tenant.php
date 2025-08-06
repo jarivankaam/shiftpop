5d66e0767f293a4e26555e5a141aa14bc845eb3c
jarivankaam <jari@jarivankaam.nl>
2025-08-06 12:27:44 +0200

    fix

--- app/Models/Tenant.php
+++ app/Models/Tenant.php
@@ -4,7 +4,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Model;
 use Stancl\Tenancy\Database\Models\Tenant as BaseTenant;
 use Illuminate\Support\Str;
-class Tenant extends Model
+class Tenant extends BaseTenant
 {
     public $incrementing = false;
     protected $keyType = 'string';
