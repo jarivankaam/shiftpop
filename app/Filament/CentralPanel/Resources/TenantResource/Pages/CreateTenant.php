de3e82133350bc1d81b0bd47dd2031d4412845e2
jarivankaam <jari@jarivankaam.nl>
2025-08-06 12:51:34 +0200

    remove dd

--- app/Filament/CentralPanel/Resources/TenantResource/Pages/CreateTenant.php
+++ app/Filament/CentralPanel/Resources/TenantResource/Pages/CreateTenant.php
@@ -29,7 +29,7 @@ protected function mutateFormDataBeforeCreate(array $data): array
         $tenant = $this->record; // ✅ This is the tenant that was just created
         // Create domain (use real domain!)
 
-        dd($tenant);
+        // dd($tenant);
         $tenant->domains()->create([
             'tenant_id' => $tenant->tenant_id,
             'domain' => "{$tenant->slug}.shiftpop.eu", // ✅ Replace "shi" with full domain
