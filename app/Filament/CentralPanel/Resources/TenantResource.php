2bd965031019c4451693cb8e43a328942e741b1c
jarivankaam <jari@jarivankaam.nl>
2025-08-06 12:19:23 +0200

    dd

--- app/Filament/CentralPanel/Resources/TenantResource.php
+++ app/Filament/CentralPanel/Resources/TenantResource.php
@@ -26,7 +26,8 @@ protected static ?string $pluralLabel = 'Tenants';
     public static function form(Forms\Form $form): Forms\Form
     {
         return $form->schema([
-            Forms\Components\TextInput::make('slug')->label('Tenant ID')->rules(['not_in:0', 'alpha_dash', 'min:3']),
+            Forms\Components\TextInput::make('slug')->label('Tenant slug')->rules(['not_in:0', 'alpha_dash', 'min:3']),
+            Forms\Components\TextInput::make('id')->required()->label('Tenant ID')->rules(['not_in:0', 'alpha_dash', 'min:3']),
         ]);
     }
 
